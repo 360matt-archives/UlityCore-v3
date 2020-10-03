daef97ee6517ce9125eced5ef07bbfcfdbd433d7
360matt <mattbacq2004@gmail.com>
2020-10-04 01:48:53 +0200

    Main load class bukkit

--- WorldManager/src/main/java/fr/ulity/worldmanager/bukkit/WorldManagerBukkit.java
+++ WorldManager/src/main/java/fr/ulity/worldmanager/bukkit/WorldManagerBukkit.java
@@ -3,8 +3,15 @@ package fr.ulity.worldmanager.bukkit;
 import fr.ulity.core_v3.Core;
 import fr.ulity.core_v3.modules.loaders.BukkitLoader;
 import fr.ulity.worldmanager.api.WorldMan;
+import fr.ulity.worldmanager.bukkit.commands.ImportDivision;
+import fr.ulity.worldmanager.bukkit.commands.WMcmd;
+import fr.ulity.worldmanager.bukkit.events.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
 
-public final class WorldManagerBukkit extends BukkitLoader {
+
+public final class WorldManagerBukkit extends BukkitLoader implements Listener {
     public static WorldManagerBukkit plugin;
 
     @Override
@@ -12,9 +19,34 @@ public final class WorldManagerBukkit extends BukkitLoader {
         plugin = this;
         Core.initialize(this);
 
+
+        registerEvent(new AnimalAttackEvent());
+        registerEvent(new AnimalInvincibleEvent());
+        registerEvent(new AnimalSpawnEvent());
+
+        registerEvent(new MobAttackEvent());
+        registerEvent(new MobInvincibleEvent());
+        registerEvent(new MobSpawnEvent());
+
+        registerEvent(new PlaceEvent());
+        registerEvent(new BrokeEvent());
+        registerEvent(new PvpEvent());
+        registerEvent(new TntEvent());
+
+        registerEvent(new PortalEnterEvent());
+
+
+        new WMcmd();
+        new ImportDivision();
+
         // reloading all
         WorldMan.reload();
 
+    }
+
+    @EventHandler
+    public void onQuit (PlayerQuitEvent e) {
+        Core.temp.remove("players." + e.getPlayer().getName());
     }
 
 }
