0c86508f76c8c52016e6f51a64c75c5659e77ee7
360matt <mattbacq2004@gmail.com>
2020-10-04 01:37:22 +0200

    Can now faster unregisterEvent

--- UlityCore_v3/src/java/fr.ulity.core_v3/modules/loaders/BukkitLoader.java
+++ UlityCore_v3/src/java/fr.ulity.core_v3/modules/loaders/BukkitLoader.java
@@ -1,6 +1,7 @@
 package fr.ulity.core_v3.modules.loaders;
 
 import org.bukkit.Bukkit;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -8,4 +9,6 @@ public class BukkitLoader extends JavaPlugin {
     public void registerEvent(Listener event) {
         Bukkit.getPluginManager().registerEvents(event, this);
     }
+    public void unregisterEvent (Listener event) { HandlerList.unregisterAll(event); }
+
 }
